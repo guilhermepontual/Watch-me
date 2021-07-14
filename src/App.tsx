0e5d7999ab8a80a5a88f27326b1927e7e243f916
Guilherme pontual <guilhermeeli2009@hotmail.com>
2021-07-14 17:31:03 -0300

    desafio concluido

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { MoviesProvider } from './MoviesContext';
+
+
+
+export function App() {
+
+
+  return (
+    <MoviesProvider>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <SideBar />
+        <Content />
+      </div>
+    </MoviesProvider>
+  )
+}
